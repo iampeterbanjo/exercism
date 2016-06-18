320e12570f0d0a8c21471dd9a18112004434a9fa
iampeterbanjo <peterbanjo@gmail.com>
2016-06-18 18:40:59 +0100

    passing most tests for go Clock using time.Time

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,8 +1,11 @@
 package clock
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-const testVersion = 3
+const testVersion = 4
 
 // Clock has minutes
 type Clock struct {
@@ -11,17 +14,30 @@ type Clock struct {
 
 // New Clock with hour and minute
 func New(hour, minute int) Clock {
+	// fmt.Printf("Passed: hour %d, minute %d \n", hour, minute)
+	t := time.Time{}
+	a := t.Add(time.Duration(hour) * time.Hour)
+	b := a.Add(time.Duration(minute) * time.Hour)
+
 	c := Clock{hour, minute}
+	c.hour, c.minute, _ = b.Clock()
+	// fmt.Printf("Return: c.hour %d, c.minute %d \n", c.hour, c.minute)
+
 	return c
 }
 
 // Clock to string
 func (c Clock) String() string {
-	return fmt.Sprintf("%d:%d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
 }
 
 // Add minutes to Clock
 func (c Clock) Add(minutes int) Clock {
-	c.minute += minutes
+	t := time.Time{}
+	x := t.Add(time.Duration(c.hour) * time.Hour)
+	y := x.Add(time.Duration(c.minute) * time.Minute)
+	z := y.Add(time.Duration(minutes) * time.Minute)
+
+	c.hour, c.minute, _ = z.Clock()
 	return c
 }
